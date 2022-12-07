fe8c00504dde92adc9dac30e71bc6c83c0bf2c17
Dmitry Sukharev <dmitri.sukharev@gmail.com>
2022-12-07 23:16:29 +0300

    add models User and Follow; UserAdmin

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField('Адрес электронной почты', unique=True)
+    password = models.CharField('Пароль', max_length=150)
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+    favorites = models.ManyToManyField(
+        'recipes.Recipe',
+        related_name='user_favorites',
+        verbose_name='Избранные рецепты'
+    )
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username[:50]
+
+
+class Follow(models.Model):
+    """Подписки на авторов - связь между пользователем и автором."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Пользователь, который подписывается'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор, на которого подписываются'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'], name='unique_user_author_pair')
+        ]
+
+    def __str__(self):
+        return f'Author {self.author.username} - User {self.user.username}'
