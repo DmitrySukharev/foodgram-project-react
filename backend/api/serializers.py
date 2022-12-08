8e1b6c92a883e5088a5c2c2a0ef9f919b8a0db1b
Dmitry Sukharev <dmitri.sukharev@gmail.com>
2022-12-09 00:21:46 +0300

    add recipe endpoint with base read serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, IngredientInRecipe
+from recipes.models import Recipe, ShoppingCart, Tag
+from users.serializers import CustomUserSerializer
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -15,3 +17,42 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name', 'measurement_unit')
+
+
+class IngredientInRecipeSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='ingredient.id')
+    name = serializers.CharField(source='ingredient.name')
+    measurement_unit = serializers.CharField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = IngredientInRecipe
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(read_only=True, many=True)
+    author = CustomUserSerializer(read_only=True)
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+    ingredients = IngredientInRecipeSerializer(
+        source='ingredientinrecipe_set',
+        many=True
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'text', 'image', 'cooking_time',
+                  'tags', 'author', 'ingredients',
+                  'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, obj):
+        return obj.user_favorites.exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        request = self.context['request']
+        user = request.user
+        if not user.is_authenticated:
+            return False
+        return ShoppingCart.objects.filter(recipe=obj, user=user).exists()
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions, viewsets
 
-from recipes.models import Ingredient, Tag
-from .serializers import IngredientSerializer, TagSerializer
+from recipes.models import Ingredient, Recipe, Tag
+from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -15,4 +15,14 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     """Получение списка ингредиентов / информации об ингредиенте по id."""
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """
+    Получение списка рецептов;
+    просмотр / добавление / обновление / удаление рецепта.
+    """
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
     permission_classes = (permissions.AllowAny,)
