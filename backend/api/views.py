1ad00396877018232e349e1889ed8deab6aca419
Dmitry Sukharev <dmitri.sukharev@gmail.com>
2022-12-11 18:12:43 +0300

    add @action for ShoppingCart

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 from rest_framework.views import APIView
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe, ShoppingCart, Tag
 from users.models import Follow
 from .permissions import AuthorOrReadOnly
 from .serializers import (CustomUserExtendedSerializer, IngredientSerializer,
@@ -67,6 +67,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
             if not already_in_favorites:
                 raise ValidationError({'errors:': 'Не было в избранном.'})
             user.favorites.remove(recipe)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=['post', 'delete'],
+            permission_classes=[permissions.IsAuthenticated])
+    def shopping_cart(self, request, pk=None):
+        user = request.user
+        recipe = self.get_object()
+        already_in_shopping_cart = ShoppingCart.objects.filter(user=user, recipe=recipe)
+        if request.method == 'POST':
+            if already_in_shopping_cart:
+                raise ValidationError({'errors:': 'Уже есть в списке покупок'})
+            ShoppingCart.objects.create(user=user, recipe=recipe)
+            serializer = RecipeMinifiedSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            if not already_in_shopping_cart:
+                raise ValidationError({'errors:': 'Не было в списке покупок.'})
+            ShoppingCart.objects.filter(user=user, recipe=recipe).delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
 
